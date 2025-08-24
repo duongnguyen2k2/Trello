1ef0a9e0cd965a9054be8471b72a5f90ecf90bbb
Lê Nguyễn Viết Dương <Lenguyenvietduong2002@gmail.com>
2025-08-24 20:58:28 +0700

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 import Button from "@mui/material/Button";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-
+import Typography from "@mui/material/Typography";
 function App() {
   return (
     <>
       <div>duongnguyen2k2</div>
+      <Typography variant="body2" color="text.secondary">
+        Test Typography
+      </Typography>
       <Button variant="contained">Hello world</Button>
       <AccessAlarmIcon />
       <ThreeDRotation />
